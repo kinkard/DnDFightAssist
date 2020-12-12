e746efbe3c370d31a5c5329d275675d07b2a94c4
Stepan Kizim <stsiapan.kizim@gmail.com>
2020-12-12 14:27:37 +0300

    Initial Commit

---
+++ DnDFightAssist/DnDFightAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  DnDFightAssistApp.swift
+//  DnDFightAssist
+//
+//  Created by Stepan Kizim on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct DnDFightAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
