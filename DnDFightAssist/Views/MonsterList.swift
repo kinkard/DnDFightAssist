210ac4f59414a243022135900a415a44f7d22bda
Stepan Kizim <stsiapan.kizim@gmail.com>
2020-12-26 19:54:56 +0300

    case insensitive search for monsters and spells

--- DnDFightAssist/Views/MonsterList.swift
+++ DnDFightAssist/Views/MonsterList.swift
@@ -13,7 +13,7 @@ struct MonsterList: View {
 
     var filteredMonsters: [Monster] {
         modelData.compendium.monsters.filter { monster in
-            (filter.isEmpty || monster.name.contains(filter))
+            (filter.isEmpty || monster.name.lowercased().contains(filter.lowercased()))
         }
     }
 
