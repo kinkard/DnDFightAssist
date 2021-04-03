df2c072e9e4bca954950dac300392adeff607719
Kinkard <stsiapan.kizim@gmail.com>
2021-04-03 22:21:42 +0300

    fixed labels edit and removed redundant VStack

--- DnDFightAssist/Model/Label.swift
+++ DnDFightAssist/Model/Label.swift
@@ -1,20 +1,13 @@
 import Foundation
 import SwiftUI // for Color
 
-class Label : Identifiable {
+struct Label : Identifiable {
     var id: Int
     var color: Color
     var text: String
     var selected: Bool
 
-    init() {
-        id = 0
-        color = .red
-        text = ""
-        selected = false
-    }
-    
-    init(id: Int, color: Color, text: String, selected: Bool) {
+    init(id: Int = 0, color: Color = .red, text: String = "", selected: Bool = false) {
         self.id = id
         self.color = color
         self.text = text
--- DnDFightAssist/Views/Labels/LabelEdit.swift
+++ DnDFightAssist/Views/Labels/LabelEdit.swift
@@ -7,39 +7,37 @@ struct LabelEdit: View {
     private let colors: [Color] = [.red, .orange, .yellow, .green, .blue, .gray]
 
     var body: some View {
-        VStack {
-            List {
-                HStack {
-                    TextField("Enter label text", text: $label.text)
-                        .padding(.leading, 8)
-                    Spacer()
-                    Button(action: {
-                        label.text = ""
-                    }) {
-                        Image(systemName: "xmark.circle.fill")
-                            .foregroundColor(.secondary)
-                            .padding(.trailing, 8)
-                            .opacity(label.text.isEmpty ? 0 : 1)
-                    }
-                }
-                .cornerRadius(5)
-
-                ForEach(colors, id: \.self) { color in
-                    HStack {
-                        Spacer()
-                        Image(systemName: "checkmark")
-                            .padding()
-                            .opacity(color == label.color ? 1 : 0)
-                    }
-                    .background(color)
-                    .cornerRadius(5)
-                    .onTapGesture {
-                        label.color = color
-                    }
+        List {
+            HStack {
+                TextField("Enter label text", text: $label.text)
+                    .padding(.leading, 8)
+                Spacer()
+                Button(action: {
+                    label.text = ""
+                }) {
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundColor(.secondary)
+                        .padding(.trailing, 8)
+                        .opacity(label.text.isEmpty ? 0 : 1)
                 }
             }
-            .listStyle(PlainListStyle())
+            .cornerRadius(5)
+
+            ForEach(colors, id: \.self) { color in
+                HStack {
+                    Spacer()
+                    Image(systemName: "checkmark")
+                        .padding()
+                        .opacity(color == label.color ? 1 : 0)
+                }
+                .background(color)
+                .cornerRadius(5)
+                .onTapGesture {
+                    label.color = color
+                }
+            }
         }
+        .listStyle(PlainListStyle())
         .navigationBarItems(
             leading: Button(action: {
                 presentationMode.wrappedValue.dismiss()
