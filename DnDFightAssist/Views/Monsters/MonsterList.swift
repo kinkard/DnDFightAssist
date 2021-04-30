6c739403a85017e710ae880184cd7e064a62d264
Kinkard <stsiapan.kizim@gmail.com>
2021-04-30 23:35:01 +0300

    fixed `Attempt to present ... which is already presenting`

--- DnDFightAssist/Views/Monsters/MonsterList.swift
+++ DnDFightAssist/Views/Monsters/MonsterList.swift
@@ -1,11 +1,9 @@
 import SwiftUI
 
 struct MonsterList: View {
-    @Environment(\.managedObjectContext) private var moc
     @EnvironmentObject private var compendium: Compendium
 
     @State private var filter: String = ""
-    @State private var showLabels = false
 
     var filteredMonsters: [Monster] {
         compendium.monsters.filter { monster in
@@ -19,18 +17,6 @@ struct MonsterList: View {
                 ForEach(filteredMonsters, id: \.name) { monster in
                     NavigationLink(destination: MonsterDetail(monster: monster)) {
                         MonsterRow(monster: monster)
-                            .contextMenu(ContextMenu(menuItems: {
-                                Button(action: {
-                                    showLabels = true
-                                }) {
-                                    Text("Labels")
-                                    Image(systemName: "tag")
-                                }
-                            }))
-                            .sheet(isPresented: $showLabels) {
-                                  LabelsModal(show: $showLabels, key: monster.name)
-                                      .environment(\.managedObjectContext, self.moc)
-                            }
                     }
                 }
             }
--- DnDFightAssist/Views/Monsters/MonsterRow.swift
+++ DnDFightAssist/Views/Monsters/MonsterRow.swift
@@ -1,7 +1,11 @@
 import SwiftUI
 
 struct MonsterRow: View {
+    @Environment(\.managedObjectContext) private var moc
+
     let monster: Monster
+
+    @State private var showLabels = false
 
     var body: some View {
       VStack {
@@ -15,6 +19,18 @@ struct MonsterRow: View {
               Spacer()
         }
         LabelsRow(key: monster.name)
+      }
+      .contextMenu(ContextMenu(menuItems: {
+          Button(action: {
+              showLabels = true
+          }) {
+              Text("Labels")
+              Image(systemName: "tag")
+          }
+      }))
+      .sheet(isPresented: $showLabels) {
+            LabelsModal(show: $showLabels, key: monster.name)
+                .environment(\.managedObjectContext, self.moc)
       }
     }
 }
