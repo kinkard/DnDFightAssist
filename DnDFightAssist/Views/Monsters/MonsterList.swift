d54fdfe7bc9bbd584c9ba34fca521d44edad151b
Kinkard <stsiapan.kizim@gmail.com>
2021-05-04 19:16:07 +0300

    added 'tag' button in search bar for MonsterList to show monsters with labels only

--- DnDFightAssist/Views/Monsters/MonsterList.swift
+++ DnDFightAssist/Views/Monsters/MonsterList.swift
@@ -4,15 +4,23 @@ struct MonsterList: View {
     @EnvironmentObject private var compendium: Compendium
 
     @State private var filter: String = ""
+    @State private var showLabelsOnly: Bool = false
+
+    @FetchRequest(
+        entity: LabelKey.entity(),
+        sortDescriptors: [],
+        predicate: NSPredicate(format: "labels.@count > 0"))
+    private var withLabels: FetchedResults<LabelKey>
 
     var filteredMonsters: [Monster] {
         compendium.monsters.filter { monster in
+          (!showLabelsOnly || withLabels.contains(where: {$0.name == monster.name})) &&
             (filter.isEmpty || monster.Matches(filter))
         }
     }
 
     var body: some View {
-        SearchNavigation(text: $filter) {
+        SearchNavigation(text: $filter, labelsOnly: $showLabelsOnly) {
             List {
                 ForEach(filteredMonsters, id: \.name) { monster in
                     NavigationLink(destination: MonsterDetail(monster: monster)) {
--- DnDFightAssist/Views/SearchNavigation.swift
+++ DnDFightAssist/Views/SearchNavigation.swift
@@ -3,6 +3,7 @@ import SwiftUI
 // originated from https://medium.com/@yugantarjain/implement-native-search-bar-in-swiftui-664a6b944538
 struct SearchNavigation<Content: View>: UIViewControllerRepresentable {
     @Binding var text: String
+    var labelsOnly: Binding<Bool>?
     var content: () -> Content
 
     func makeUIViewController(context: Context) -> UINavigationController {
@@ -19,19 +20,28 @@ struct SearchNavigation<Content: View>: UIViewControllerRepresentable {
     }
 
     func makeCoordinator() -> Coordinator {
-        Coordinator(content: content(), searchText: $text)
+        Coordinator(content: content(), searchText: $text, labelsOnly: labelsOnly)
     }
 
     class Coordinator: NSObject, UISearchBarDelegate {
         @Binding var text: String
+        var _labelsOnly: Binding<Bool>?
         let rootViewController: UIHostingController<Content>
         let searchController = UISearchController(searchResultsController: nil)
 
-        init(content: Content, searchText: Binding<String>) {
+        private var toggled: Bool = false
+
+      init(content: Content, searchText: Binding<String>, labelsOnly: Binding<Bool>?) {
             rootViewController = UIHostingController(rootView: content)
             searchController.searchBar.autocapitalizationType = .none
             searchController.obscuresBackgroundDuringPresentation = false
             rootViewController.navigationItem.searchController = searchController
+
+          _labelsOnly = labelsOnly
+          if (_labelsOnly != nil) {
+            searchController.searchBar.showsBookmarkButton = true
+            searchController.searchBar.setImage(UIImage(systemName: "tag"), for: .bookmark, state: .normal)
+          }
 
             _text = searchText
         }
@@ -47,6 +57,17 @@ struct SearchNavigation<Content: View>: UIViewControllerRepresentable {
 
         func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
             text = ""
+        }
+
+        func searchBarBookmarkButtonClicked(_ searchBar: UISearchBar) {
+          toggled.toggle()
+
+          let img = UIImage(systemName: toggled ? "tag.fill" : "tag")
+          searchBar.setImage(img, for: .bookmark, state: .normal)
+          
+          if (_labelsOnly != nil) {
+            _labelsOnly!.wrappedValue = toggled
+          }
         }
     }
 }
